0eff5bbb39ddcf51148720bea6bac45b8e16c5a7
mdshantosharker <mdshantosharker@gmail.com>
2026-04-09 17:45:34 +0600

    added navbar

--- src/ui/ErrorPage/ErrorPage.jsx
+++ src/ui/ErrorPage/ErrorPage.jsx
@@ -5,6 +5,7 @@ const ErrorPage = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
+        <img src="/src/assets/App-Error.png" alt="" />
         <h1 className="text-6xl font-bold text-red-500">404</h1>
 
         <p className="mt-4 text-gray-600">Page Not Found</p>
